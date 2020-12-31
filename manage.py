5d93ab4b94db34c383b27084d1f7fba7c636ea39
EmberOn <ember.contactme@gmail.com>
2020-12-31 02:04:58 +0100

    fix manage.py environment variable setting

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('asfix.settings', 'asfix.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asfix.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
